c6ee8c39a40cb147e6552fb60e14d85772d7aa4a
Lourdesrsdp <lourdesrsdp@gmail.com>
2023-04-04 21:22:24 +0200

    Setting up application to use parcel

---
+++ src/nevera.js
@@ -0,0 +1,13 @@
+/* import { productTypes } from "ProductTypes";
+
+productTypes;
+
+for (var i = 0; i < productTypes.length; i++) {
+    var opt = productTypes[i];
+
+    var el = document.createElement("modelSelect");
+    el.text = opt;
+    el.value = opt;
+
+    select.add(el);
+}    */
