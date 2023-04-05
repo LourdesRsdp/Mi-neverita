76cad6d40550739ab1fc715d775c1d38af6be792
Lourdesrsdp <lourdesrsdp@gmail.com>
2023-04-05 23:52:09 +0200

    Selectors created

--- src/index.js
+++ src/index.js
@@ -2,3 +2,28 @@ import { productTypes } from "./Models/ProductTypes";
 import { AddOptionsToSelect } from "./Utils/utils";
 
 AddOptionsToSelect("productTypes", productTypes);  
+
+function AddEventToSelect(selectId) {
+    
+    const select = document.getElementById(selectId);
+    select.addEventListener("change", event => {
+        const selector2 = document.createElement("select");
+        const container = document.getElementById("selector");
+        container.appendChild(selector2);
+    });
+    
+};
+
+AddEventToSelect("productTypes");
+
+
+
+
+
+
+
+
+
+
+
+
