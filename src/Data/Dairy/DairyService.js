ad07bf0e2272b888ec702dce0c149d6f4c19f3b2
Carlos Pantoja Gomez <carlos.pantoja.gomez@mercell.com>
2023-04-06 11:44:46 +0200

    Creating services

---
+++ src/Data/Dairy/DairyService.js
@@ -0,0 +1,7 @@
+import { data } from "./Data";
+
+const getDairies = () => {
+    return data;
+}
+
+export{getDairies};
---
+++ src/Data/Dairy/Data.js
@@ -0,0 +1,39 @@
+const data = [
+    {
+        id: 1,
+        name: "Milk",
+        size: 1,
+        color: "White",
+        price: 1
+    },
+    {
+        id: 2,
+        name: "Cheese",
+        size: 1,
+        color: "Yellow",
+        price: 4
+    },
+    {
+        id: 3,
+        name: "Yogurt",
+        size: 1,
+        color: "White",
+        price: 2
+    },
+    {
+        id: 4,
+        name: "Butter",
+        size: 1,
+        color: "Yellow",
+        price: 3
+    },
+    {
+        id: 5,
+        name: "Sour Cream",
+        size: 1,
+        color: "White",
+        price: 2
+    }
+];
+
+export {data};
