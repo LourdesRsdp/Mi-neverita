2b5d197e575bdf9cb849f8cd2254f4249d08fcf8
CarlosPantojaGomez <carlos.pantoja.gomez@mercell.com>
2023-10-25 19:41:44 +0200

    creating fake db

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { BoardComponent } from '../../components/board/board.component';
+import { ProductService } from 'src/app/services/product/product.service';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +8,17 @@ import { BoardComponent } from '../../components/board/board.component';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  
+  products: any[] = [];
+
+  constructor(
+    private productService: ProductService
+  ) { }
+
+  ngOnInit() {
+    this.productService.getProducts().subscribe(products => {
+      this.products = products as any[];
+      console.log(this.products);
+      
+    });
+  }
 }
