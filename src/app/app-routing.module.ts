4a641edf0d2a74ae62f944011d458f11fe09e49c
Lourdesrsdp <lourdesrsdp@gmail.com>
2023-10-25 12:05:53 +0200

    duys rooting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
+import { RecipesComponent } from './views/recipes/recipes.component';
+import { MarketplaceComponent } from './views/marketplace/marketplace.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'recipes', component: RecipesComponent },
+  { path: 'marketplace', component: MarketplaceComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }];
 
 @NgModule({
