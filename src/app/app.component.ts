ace5655f86d8edff0bf987f576d658ba32c37988
CarlosPantojaGomez <carlos.pantoja.gomez@mercell.com>
2023-10-25 13:08:53 +0200

    creating link buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'mi-neverita';
+
+  constructor(
+    private router: Router
+  ) { }
+
+  public goTo(url: string): void {
+    this.router.navigate([url]);
+  }
 }
+
+
+
+
